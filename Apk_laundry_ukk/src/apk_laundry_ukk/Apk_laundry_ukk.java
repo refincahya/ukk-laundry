c69b7b9830c784054c225c472c20c66c340a397c
refincahya <144768387+refincahya@users.noreply.github.com>
2023-10-16 08:41:11 +0700

    Add files via upload

---
+++ Apk_laundry_ukk/src/apk_laundry_ukk/Apk_laundry_ukk.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apk_laundry_ukk;
+
+/**
+ *
+ * @author Rohim
+ */
+public class Apk_laundry_ukk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
